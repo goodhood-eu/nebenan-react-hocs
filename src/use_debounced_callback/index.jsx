073d364c8c6397f44cdf00e24bbf791bf47bd91d
Peter Haßler <pet.hassler@gmail.com>
2021-01-14 11:09:17 +0100

    Change debounce and throttle hooks to finish execution before creating a new instance

--- src/use_debounced_callback/index.jsx
+++ src/use_debounced_callback/index.jsx
@@ -1,11 +1,18 @@
 import debounce from 'lodash/debounce';
-import { useMemo } from 'react';
+import { useEffect, useState } from 'react';
 
-const useDebouncedCallback = (func, delay) => (
-  useMemo(
-    () => debounce(func, delay),
-    [func, delay],
-  )
-);
+const useDebouncedCallback = (func, delay) => {
+  const [instance, setInstance] = useState(undefined);
+
+  useEffect(() => {
+    const debounced = debounce(func, delay);
+
+    setInstance(debounced);
+
+    return () => { debounced.flush(); };
+  }, [func, delay]);
+
+  return instance;
+};
 
 export default useDebouncedCallback;
--- src/use_throttled_callback/index.jsx
+++ src/use_throttled_callback/index.jsx
@@ -1,11 +1,18 @@
 import throttle from 'lodash/throttle';
-import { useMemo } from 'react';
+import { useEffect, useState } from 'react';
 
-const useThrottledCallback = (func, delay) => (
-  useMemo(
-    () => throttle(func, delay),
-    [func, delay],
-  )
-);
+const useThrottledCallback = (func, delay) => {
+  const [instance, setInstance] = useState(undefined);
+
+  useEffect(() => {
+    const throttled = throttle(func, delay);
+
+    setInstance(throttled);
+
+    return () => { throttled.flush(); };
+  }, [func, delay]);
+
+  return instance;
+};
 
 export default useThrottledCallback;

386c9cac63bad2fc50134a089d648576b11b2129
Peter Haßler <pet.hassler@gmail.com>
2021-01-25 10:18:10 +0100

    Fix issue with options deps and remove need to re-attach listener after fn change

--- src/use_event_listener/index.jsx
+++ src/use_event_listener/index.jsx
@@ -1,19 +1,26 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 export const useEventListener = (
   ref,
   type,
-  listener,
-  options = { passive: true },
+  handler,
+  { passive, capture, once } = { passive: true },
 ) => {
+  const node = ref.current;
+  const handlerRef = useRef(null);
+
+  handlerRef.current = handler;
+
   useEffect(() => {
-    const node = ref.current;
     if (!node) return;
+
+    const listener = (...args) => handlerRef.current(...args);
+    const options = { passive, capture, once };
 
     node.addEventListener(type, listener, options);
 
     return () => {
       node.removeEventListener(type, listener, options);
     };
-  }, [type, listener, options]);
+  }, [type, node, passive, capture, once]);
 };

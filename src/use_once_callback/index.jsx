629a51601f1ca330c9df1b23f5271080e910f8fa
Peter Haßler <office.hassler@gmail.com>
2021-10-20 11:31:35 +0200

    Add useOnceCallback

---
+++ src/use_once_callback/index.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useRef } from 'react';
+import useStableCallback from '../use_stable_callback';
+
+/**
+ * @param fn function to be called once
+ * @param {any[]} resetDeps array of dependencies for resetting 'once' state
+ * @returns {function} callback with stable identity
+ */
+const useOnceCallback = (fn, resetDeps = []) => {
+  const wasCalledRef = useRef(false);
+
+  useEffect(() => {
+    wasCalledRef.current = false;
+  }, resetDeps);
+
+  return useStableCallback((...args) => {
+    if (wasCalledRef.current) return;
+
+    fn(...args);
+    wasCalledRef.current = true;
+  });
+};
+
+export default useOnceCallback;
